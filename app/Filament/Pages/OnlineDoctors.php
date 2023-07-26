f0ba8a56594796bc3ae70eb0f0a1a2d8aacb53e8
ayatamine <ayatir04@gmail.com>
2023-07-26 10:12:44 +0100

    add online doctrs page

--- app/Filament/Pages/OnlineDoctors.php
+++ app/Filament/Pages/OnlineDoctors.php
@@ -2,11 +2,75 @@
 
 namespace App\Filament\Pages;
 
+use Filament\Forms;
+use App\Models\Doctor;
 use Filament\Pages\Page;
+use Filament\Resources\Form;
+use Filament\Pages\Actions\Action;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Notifications\Notification;
 
 class OnlineDoctors extends Page
 {
+    use  InteractsWithForms; 
+    public $notification_title,$notification_content;
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.online-doctors';
+    protected function getViewData(): array
+    {
+       $doctors = Doctor::with('specialities:id,name')->with('sub_specialities:id,name')->whereNotificationStatus(true)->paginate(10);
+       return [
+        'doctors'=>$doctors
+       ];
+    }
+    protected function getActions(): array
+    {
+        return [
+            Action::make('Send Notification')->color('primary')
+            ->action(function (array $data): void {
+                Notification::make()
+                            ->title('Notifications send successfully to doctors')
+                            ->success() 
+                            ->duration(5000) 
+                            ->send();
+            })
+            ->form([
+                Forms\Components\TextInput::make('notification_title')
+                ->required()
+                ->label(trans('notification_title'))
+                ->maxLength(150),
+                Forms\Components\Textarea::make('notification_content')
+                    ->required()
+                    ->columnSpan('full')
+                    ->maxLength(16777),
+                    
+            ])
+        ];
+    }
+   protected function getFormSchema(): array
+    {  
+        return [
+            Section::make('Send Notification')
+                ->schema([
+                    Forms\Components\TextInput::make('notification_title')
+                    ->required()
+                    ->label(trans('notification_title'))
+                    ->maxLength(150),
+                    Forms\Components\Textarea::make('notification_content')
+                        ->required()
+                        ->columnSpan('full')
+                        ->maxLength(16777),
+                        
+                ])
+                ->collapsible()
+                ->collapsed()
+                
+            ];
+    }
+    public function submit(): void
+    {
+       dd($this->form->getState());
+    }
 }
--- resources/views/filament/pages/online-doctors.blade.php
+++ resources/views/filament/pages/online-doctors.blade.php
@@ -1,3 +1,36 @@
 <x-filament::page>
-
+    {{-- <div class="flex justify-end">
+        <button class="bg-blue-400 text-white text-sm p-0.5 px-2 rounded">Send Notification</button>
+    </div> --}}
+    {{-- <form wire:submit.prevent="submit">
+        {{ $this->form }}
+     
+        <button type="submit">
+            Submit
+        </button>
+    </form> --}}
+    <div class="grid grid-cols-1 md-grid-cols-3 xl:grid-cols-4 gap-2">
+        @forelse ($doctors as $d)
+            <div class="bg-white flex items-center border p-4 rounded-lg shadow-md">
+                <div class="flex-shrink-0 mr-4">
+                <img class="h-16 w-16 rounded-full object-cover" src="{{$d->thumbnail}}" alt="{{$d->full_name}}">
+                </div>
+                <div>
+                <h4 class="font-semibold text-base">{{$d->full_name}}</h4>
+                <p class="text-gray-600">
+                    @if(count($d->specialities))
+                    {{$d->specialities[0]->name}}
+                    @elseif(count($d->sub_specialities))
+                    {{$d->sub_specialities[0]->name}}
+                    @endif
+                </p>
+                </div>
+            </div>
+        @empty
+            <span class="bg-blue-400 text-white p-2 text-base">No Speciality Presented</span>
+        @endforelse
+       
+          
+    </div>
+    {{$doctors->links()}}
 </x-filament::page>
